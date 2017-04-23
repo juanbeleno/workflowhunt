bc511aff704a9051f81441b55e1c62fe2da73180
Juan Beleño <jsbeleno@gmail.com>
2017-04-23 21:54:50 +0000

    update: graphic interface for search

---
+++ application/controllers/Web.php
@@ -0,0 +1,69 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Web extends CI_Controller {
+
+	public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('elasticsearch_model');
+    }
+
+	public function index()
+	{
+		$this->load->view('web/home');
+	}
+
+	public function results(){
+		$query = @$this->input->get('query', TRUE);
+		$method = @$this->input->get('method', TRUE);
+		$offset = @$this->uri->segment(3);
+		$offset = ($offset == '')? 0 : $offset;
+
+		$response = array();
+		
+		if($method == "semantics")
+		{
+			// TODO: replace by semantic-based search
+			$response = $this->elasticsearch_model->search_in_metadata($query, $offset);
+		}
+		else
+		{
+			$response = $this->elasticsearch_model->search_in_metadata($query, $offset);
+		}
+
+		$data['query'] = @$query;
+		$data['method'] = @$method;
+		$data['status'] = @$response['status'];
+		$data['results'] = @$response['results'];
+		$data['total'] = @$response['total'];
+		$data['msg'] = @$response['msg'];
+
+		$this->load->library('pagination');
+		$this->load->helper('text');
+
+		$config['base_url'] = base_url().'index.php/web/results/';
+		$config['total_rows'] = ($data['total'] == '')? 0 : $data['total'];
+		$config['per_page'] = 10;
+		$config['reuse_query_string'] = true;
+		$config['num_tag_open'] = '<li class="page-item">';
+		$config['num_tag_close'] = '</li>';
+		$config['first_tag_open'] = '<li class="page-item">';
+		$config['first_tag_close'] = '</li>';
+		$config['last_tag_open'] = '<li class="page-item">';
+		$config['last_tag_close'] = '</li>';
+		$config['next_tag_open'] = '<li class="page-item">';
+		$config['next_tag_close'] = '</li>';
+		$config['prev_tag_open'] = '<li class="page-item">';
+		$config['prev_tag_close'] = '</li>';
+		$config['cur_tag_open'] = '<li class="page-item">';
+		$config['cur_tag_close'] = '</li>';
+
+		$this->pagination->initialize($config);
+
+		$data['pagination'] = $this->pagination;
+
+		$this->load->view('web/results', $data);
+	}
+
+}
--- application/views/web/results.php
+++ application/views/web/results.php
@@ -40,18 +40,18 @@
 
 					<div>
 						<?php 
-							if($method == "semantic")
+							if($method == "semantics")
 							{
 						?>
 							<a href="<?php print base_url();?>index.php/web/results/?query=<?php print urlencode($query); ?>" class="results-tab">Keyword</a>
-							<a href="<?php print base_url();?>index.php/web/results/?query=<?php print urlencode($query); ?>&method=semantic" class="results-tab-active">Semantic</a>
+							<a href="<?php print base_url();?>index.php/web/results/?query=<?php print urlencode($query); ?>&method=semantics" class="results-tab-active">Semantics</a>
 						<?php
 							}
 							else
 							{ 
 						?>
 							<a href="<?php print base_url();?>index.php/web/results/?query=<?php print urlencode($query); ?>" class="results-tab-active">Keyword</a>
-							<a href="<?php print base_url();?>index.php/web/results/?query=<?php print urlencode($query); ?>&method=semantic" class="results-tab">Semantic</a>
+							<a href="<?php print base_url();?>index.php/web/results/?query=<?php print urlencode($query); ?>&method=semantics" class="results-tab">Semantics</a>
 						<?php 
 							}
 						?>
@@ -81,6 +81,7 @@
 							<li>Try using more general keywords.</li>
 							<li>Make your queries as concise as possible.</li>
 						</ul>
+						<!-- ul -->
 					</div>
 					<!-- .results-not-found -->
 				<?php 	
@@ -107,10 +108,10 @@
 										<?php print character_limiter($workflow['_source']['description'], 150);?>
 									</div>
 									<div class="results-workflow-wfms">
-										Workflow Management System: <strong><?php print $workflow['_source']['wfms'];?></strong>
+										Workflow Management System: <strong><?php print $workflow['_source']['swms'];?></strong>
 									</div>
 									<?php 
-										if($method == "semantic")
+										if($method == "semantics")
 										{
 									?>
 									<div class="results-semantic-annotations">
@@ -119,9 +120,8 @@
 											<a target="_blank" href="<?php print base_url().'index.php/web/workflow?id='.$workflow['_id']; ?>">
 												READ MORE		
 											</a>
-											 - 
-											<span class="results-span-analytics" onclick="show_analytics(<?php print $workflow['_id'];?>)">ANALYTICS</span>
 									</div>
+									<!-- .results-semantic-annotations -->
 									<?php
 										}
 									?>
@@ -145,84 +145,11 @@
 					}
 				?>
 			</div>
-			<!-- .col-lg-10 .col-xl-10 .offset-lg-2 .offset-xl-2-->
-			<div class="col-lg-4 col-xl-4" id="semantic-augmentation">
-				
-			</div>
-			<!-- .col-lg-4 .col-xl-4-->
+			<!-- .col-lg-7 .col-xl-7 .offset-lg-1 .offset-xl-1 -->
 		</div>
 	</div>
 
 	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-
-    <!-- Custom JavaScript -->
-    <script type="text/javascript">
-    	var flag = true;
-
-    	function show_analytics(id_workflow)
-    	{
-    		if(flag)
-    		{
-    			flag = false;
-    			$.ajax({
-					method: "POST",
-					url: "<?php print base_url(); ?>index.php/workflow/get_semantic_augmentation",
-					context: document.body,
-					data: { 
-						id_workflow: id_workflow
-					},
-					success: function(result){
-						if(result.status == 'OK')
-						{
-							var content = "";
-							if(result.workflows.length > 0)
-							{
-								content += '<div class="results-analytic-box">';
-								content += '<div class="results-analytic-title">Workflows associated</div>';
-								content += '<div class="results-analytic-description">';
-								content += 'These workflows were listed here because they are subworkflows of the selected workflow or they use the selected workflow as subworkflow.';
-								content += '</div>';
-								content += '<ul class="results-analytic-list">';
-								for (var i = 0; i < result.workflows.length; i++) 
-								{
-									content += '<li><a target="_blank" href="<?php print base_url().'index.php/web/workflow?id='; ?>'+result.workflows[i].id+'">'+result.workflows[i].title+'</a></li>';
-								}
-								content += '</ul>';
-								content += '</div>';
-							}
-
-							if(result.authors.length > 0)
-							{
-								content += '<div class="results-analytic-box">';
-								content += '<div class="results-analytic-title">Authors</div>';
-								for (var i = 0; i < result.authors.length; i++) 
-								{
-									content += '<div class="results-analytic-author">';
-									content += '<img class="results-analytic-author-thumbnail" src="'+result.authors[i].photo+'">';
-									content += '<div class="results-analytic-author-content">';
-									content += '<a href="https://www.myexperiment.org/users/59168">'+result.authors[i].name+'</a>';
-									if(result.authors[i].email)
-									{
-										content += '<div>Email: '+result.authors[i].email+'</div>';
-									}
-									if(result.authors[i].website)
-									{
-										content += '<div>Website: <a href="'+result.authors[i].website+'"> Here </a></div>';
-									}
-									content += '</div>';
-									content += '</div>';
-								}
-								content += '</div>';
-							}
-
-							$("#semantic-augmentation").html(content);
-						}
-						flag = true;
-		    		}
-		    	});
-    		}
-    	}
-    </script>
 </body>
 </html>
